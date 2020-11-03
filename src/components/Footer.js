3ff55d0e6eae7e5e5a48a1da7227ee0a61081d25
Jonathan Bleibdrey <jonathanbleibdrey@Jonathans-MacBook-Air.local>
2020-11-03 12:58:42 -0500

    finished up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,13 @@ function Footer() {
        <footer className='mt-5'>
 
            <Container fluid={true}>
-               <Row>
+               <Row className="border-top justify-content-between p-3">
                    <Col className='p-0' md={3} sm={12}> 
                    Footer
+                   </Col> 
+
+                   <Col className="p-0 d-flex justify-content-end" md={3}>
+                       This site was made by Jonathan Bleibdrey.
                    </Col> 
                </Row>
            </Container>
