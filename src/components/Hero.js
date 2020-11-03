f7a08b409c48670abcc2b14195ee04f57e6be6c5
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-03 14:31:19 -0500

    added font to lok better with bootstrap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,11 +8,11 @@ import Col from 'react-bootstrap/Col'
 
 function Hero(props) {
     return (
-        <Jumbotron>
-            <Container>
-                <Row>
-                    <Col>
-                    { props.title && <h1>{props.title}</h1> }
+        <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+            <Container fluid={true}>
+                <Row className="justify-content-center py-5">
+                    <Col md={4} sm={12}>
+                    { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
                     { props.subTitle && <h3>{props.subTitle}</h3> }
                     { props.text && <h3>{props.text}</h3> }
                     </Col>
