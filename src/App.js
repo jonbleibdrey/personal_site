ea3cf5812e6f9f7eb5e624571d217fb5e64f2d07
Jonathan Bleibdrey <jonathanbleibdrey@Jonathans-MacBook-Air.local>
2020-11-03 13:26:26 -0500

    conected the routes

--- src/App.js
+++ src/App.js
@@ -62,8 +62,8 @@ class App extends React.Component{
         </Navbar>
 
         <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} /> }/>
-        <Route path="/" exact render={() => <AboutPage title={this.state.about.title} /> }/>
-        <Route path="/" exact render={() => <ContactPage title={this.state.contact.title} /> }/>
+        <Route path="/about" render={() => <AboutPage title={this.state.about.title} /> }/>
+        <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} /> }/>
         <Footer/>
 
 
