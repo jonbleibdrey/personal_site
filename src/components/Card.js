de1e14320e42fda467d00a1f43bfaa828cb693b2
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-04 14:54:38 -0500

    added the pictures and cards some styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import CardInfo from '../components/CardInfo'
 
 function Card(props) {
     return (
-        <div>
-            <img className="" src={props.item.imgSrc} alt={props.item.imgSrc}/>
+        <div className='d-inline-block g-card'>
+            <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc}/>
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}/>}
         </div>
     )
