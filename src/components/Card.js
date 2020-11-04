90da22c5e9f1d45f026f98365aa879a3e549f412
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-04 14:22:03 -0500

    added cards and function to click

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card(props) {
+    return (
+       <p>
+           Card works!
+       </p>
+    )
+}
+
+export default Card
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react'
+import briefcase  from '../assets/images/briefcase.PNG'
+import candy  from '../assets/images/candy.PNG'
+import shoe  from '../assets/images/shoe.PNG'
+import Card from '../components/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 export class Carousel extends Component {
 
@@ -7,18 +13,66 @@ export class Carousel extends Component {
         this.state = {
             items: [
                 {
-                    id:0
-                    title: 
-                }
+                    id:0,
+                    title: "Linked in",
+                    subTitle: "check out my resume",
+                    imgSrc: briefcase,
+                    link: "https://www.linkedin.com/mwlite/in/jonathan-bleibdrey-4032171b9",
+                    selected: false
+
+                },
+                {
+                    id:1,
+                    title: "Github",
+                    subTitle: "check out what i have made",
+                    imgSrc: candy,
+                    link: "https://github.com/jonbleibdrey",
+                    selected: false
+
+                },
+                {
+                    id:2,
+                    title: "Facebook",
+                    subTitle: "check out my social media",
+                    imgSrc: shoe,
+                    link: "https://www.facebook.com/jbleibdrey/",
+                    selected: false
+
+                },
+
             ]
         }
     }
 
+    handleCardClick = (id,card) => {
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id != id) {
+                item.selected = false
+            }
+        })
+
+        this.setState({
+            items
+        })
+    }
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
     render() {
         return (
-            <div>
-                Carosule works
-            </div>
+            <Container fluid={true}>
+             <Row className="justify-content-around">
+                {this.makeItems(this.state.items)}
+             </Row>
+            </Container>
         )
     }
 }
