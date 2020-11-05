f1fe9c4a22c319600da536bcd4b700132d72fa99
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-05 10:19:35 -0500

    finished up about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../'
 import HomeText from '../components/HomeText'
 import Content from '../components/Content'
+import { Container } from 'react-bootstrap'
 
 function AboutPage(props) {
     return (
         <div>
             <HomeText title={props.title}/>
             <Content>
+                <Container>
                <p>Hello, my name is Jonathan. I'm a full stack engineer with experience in Ruby, Rails, Javascript, React and Redux.</p>  
               
                <p>My dream is to show people that you are left and right brain, that we have the capability to be smart and creative. </p>  
@@ -16,7 +18,8 @@ function AboutPage(props) {
                
                <p>My last couple of projects, where based on the economics of where I think the world is going during the Covid-19 times. You can find them<a href="https://github.com/jonbleibdrey"> here. </a></p>  
                
-               <p>When I'm not learning something new chances are I'm creating some type of art. Which you can find or here </p>  
+               <p>When I'm not learning something new chances are I'm creating some type of art. Which you can find photography <a href="https://www.instagram.com/johnnyb.experience/"> here. </a> or drawings <a href="https://www.instagram.com/johnnyb.exploration/"> here. </a> </p>  
+               </Container>
             </Content>
         </div>
     )
