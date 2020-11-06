60ec096764ff0cc8685af0a2f1d9c7dfc3ea1806
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-06 11:19:08 -0500

    added text to about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,11 +15,11 @@ function AboutPage(props) {
                 <Container>
                <p>Hello, my name is Jonathan. I'm a full stack engineer with experience in Ruby, Rails, Javascript, React and Redux.</p>  
               
-               <p>My dream is to show people that you are left and right brain, that we have the capability to be smart and creative. </p>  
+               <p>My dream is to show people that we are naturally left and right brain, that we have the capability to be smart and creative. </p>  
                
                <p>I'm constantly learning new things. currently those things include gaining more experience with Javascript, React and Redux</p>  
                
-               <p>My last couple of projects, where based on the economics of where I think the world is going during the Covid-19 times. You can find them<a href="https://github.com/jonbleibdrey"> here. </a></p>  
+               <p>My last couple of projects, where based on the economics of where I think the world is going, during the Covid-19 period. You can find them<a href="https://github.com/jonbleibdrey"> here. </a></p>  
                
                <p>When I'm not learning something new chances are I'm creating some type of art. Which you can find photography <a href="https://www.instagram.com/johnnyb.experience/"> here. </a> or drawings <a href="https://www.instagram.com/johnnyb.exploration/"> here. </a> </p>  
                </Container>
