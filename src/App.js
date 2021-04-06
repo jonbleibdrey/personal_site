b73d741fe8326158c3ec4ee3418461d1827cd231
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-06 13:47:25 -0400

    adding new project link

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ class App extends React.Component{
                   </Nav>
                 </NavbarCollapse>
           </Navbar>
+          
             <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} /> }/>
             <Route path="/about" render={() => <AboutPage title={this.state.about.title} /> }/>
             <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} /> }/>
