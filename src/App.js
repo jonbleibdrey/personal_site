b1c58c0d99dc0c517b33938e633c3900b514bebd
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-20 10:24:37 -0400

    cleaned up app with routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
+import Routers from "./components/Routers";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import PortfolioPage from "./pages/PortfolioPage";
@@ -44,30 +44,13 @@ class App extends React.Component {
           <div className="app__div">
             <NavBar />
             <Switch>
-              <Route
-                path="/"
-                exact
-                render={() => (
-                  <HomePage
-                    title={this.state.home.title}
-                    subTitle={this.state.home.subTitle}
-                    text={this.state.home.text}
-                  />
-                )}
-              />
-              <Route
-                path="/about"
-                render={() => <AboutPage title={this.state.about.title} />}
-              />
-              <Route
-                path="/contact"
-                render={() => <ContactPage title={this.state.contact.title} />}
-              />
-              <Route
-                path="/portfolio"
-                render={() => (
-                  <PortfolioPage title={this.state.portfolio.title} />
-                )}
+              <Routers
+                homeTitle={this.state.home.title}
+                homeSubTitle={this.state.home.subTitle}
+                homeText={this.state.home.text}
+                aboutTitle={this.state.about.title}
+                contactTitle={this.state.contact.title}
+                portTitle={this.state.portfolio.title}
               />
             </Switch>
             <AboutPage name="aboutPage" />
