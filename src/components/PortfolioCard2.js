e73e6d8ebee10339881180260ca87607e197995a
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-24 15:07:11 -0400

    cleaned up portfolio card 1, working on the next

--- src/components/PortfolioCard2.js
+++ src/components/PortfolioCard2.js
@@ -11,17 +11,34 @@ const PortfolioCard2 = () => {
       {
         link: "https://blissful-northcutt-caae64.netlify.app/",
         img: `${Todo}`,
-        imgText: "Read By Humans",
+        imgText: "To-do list",
         text: " Created a simple site for a todo list. Built with react, javascript, and common css for styling. wanted to use mostly hooks to make this come alive.",
       },
-      {},
-      {},
+      {
+        link: "https://eloquent-benz-ba9740.netlify.app/",
+        img: `${ReadByHumans}`,
+        imgText: "Read By Humans",
+        text: " Created a site to search for any book from Google book API. Built with react and google API, used bootstrap and common css for styling. Used npm smooth scroll for one continous page. Used Axios for my fetch calls as well.",
+      },
+      {
+        link: "https://github.com/jonbleibdrey/rails_food_truck_review",
+        img: `${FoodTruck}`,
+        imgText: "FoodTruck app",
+        text: " Food Truck Review is my Rails portfolio project for the Flatiron coding bootcamp. It is an application for users to view, add, and reviewing food trucks. In this project, I tried Test Driven Development for the first time, implemented dynamic SQL searching capability, and integrated GitHub oauth.",
+      },
+      {
+        link: "https://github.com/jonbleibdrey/the_spiritual_thought",
+        img: `${Journal}`,
+        imgText: "Spiritual Thought",
+        text: "This was my second portfolio project EVER, we all have to start somewhere. It's meant to demonstrate my competence with the Sinatra web framework. I built a journal application for managing users journals and if you wanted to share with other people you could do that also.",
+      },
     ],
   });
 
+  console.log(setCardInfo)
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      
       {cardInfo.info.map((info) => (
         <Card
           className="portfolio__card"
@@ -53,121 +70,6 @@ const PortfolioCard2 = () => {
           </Card.Body>
         </Card>
       ))}
-
-      <Card
-        className="portfolio__card"
-        style={{
-          margin: "1%",
-          marginBottom: "10%",
-          paddingBottom: "5%",
-          boxShadow: "-10px 12px 20px 13px #ccc",
-          borderRadius: "15px",
-        }}
-        //   could not style with css
-      >
-        <a
-          href="https://eloquent-benz-ba9740.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Card.Img
-            variant="top"
-            src={ReadByHumans}
-            alt="Read By Humans"
-            style={{
-              maxHeight: "400px",
-              minHeight: "200px",
-              borderRadius: "15px",
-            }}
-            //   could not style with css
-          />
-        </a>
-        <Card.Body className="portfolio__cardBody">
-          <Card.Title>Read By Humans</Card.Title>
-          <Card.Text>
-            Created a site to search for any book from Google book API. Built
-            with react and google API, used bootstrap and common css for
-            styling. Used npm smooth scroll for one continous page. Used Axios
-            for my fetch calls as well.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card
-        style={{
-          margin: "1%",
-          marginBottom: "10%",
-          boxShadow: "-10px 12px 20px 13px #ccc",
-          borderRadius: "15px",
-        }}
-        //   could not style with css
-      >
-        <a
-          href="https://github.com/jonbleibdrey/rails_food_truck_review"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Card.Img
-            variant="top"
-            src={FoodTruck}
-            alt="foodtruck app"
-            style={{
-              maxHeight: "400px",
-              minHeight: "200px",
-              borderRadius: "15px",
-            }}
-            //   could not style with css
-          />
-        </a>
-        <Card.Body className="portfolio__cardBody">
-          <Card.Title>Food Truck</Card.Title>
-          <Card.Text>
-            Food Truck Review is my Rails portfolio project for the Flatiron
-            coding bootcamp. It is an application for users to view, add, and
-            reviewing food trucks. In this project, I tried Test Driven
-            Development for the first time, implemented dynamic SQL searching
-            capability, and integrated GitHub oauth.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card
-        style={{
-          margin: "1%",
-          marginBottom: "10%",
-          boxShadow: "-10px 12px 20px 13px #ccc",
-          borderRadius: "15px",
-        }}
-        //   could not style with css
-      >
-        <a
-          href="https://github.com/jonbleibdrey/the_spiritual_thought"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Card.Img
-            variant="top"
-            src={Journal}
-            alt="journal app"
-            style={{
-              maxHeight: "400px",
-              minHeight: "200px",
-              borderRadius: "15px",
-            }}
-            //   could not style with css
-          />
-        </a>
-        <Card.Body className="portfolio__cardBody">
-          <Card.Title>The Spiritual Thought</Card.Title>
-          <Card.Text>
-            This was my second portfolio project EVER, we all have to start
-            somewhere. It's meant to demonstrate my competence with the Sinatra
-            web framework. I built a journal application for managing users
-            journals and if you wanted to share with other people you could do
-            that also.
-          </Card.Text>
-        </Card.Body>
-      </Card>
     </div>
   );
 };
