b87cc31c164a8daaf4da66a4fa54d175735cfe71
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-01-19 15:38:02 -0500

    cleared error for push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import React from "react";
+// import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 
 const App = () => {
